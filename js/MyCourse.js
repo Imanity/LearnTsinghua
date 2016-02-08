9ffeab52d1c7cba1d97de1d780a5ccb58a5e0f65
唐人杰 <imanity14@126.com>
2016-02-08 15:40:25 +0800

    Main UI modified

--- js/MyCourse.js
+++ js/MyCourse.js
@@ -9,5 +9,11 @@ chrome.extension.sendMessage({cmd: "isLearnPageActivated"}, function (response) 
     newElement.disabled=false;
     var head=document.getElementsByTagName("head")[0];
     head.appendChild(newElement);
+    $('#info_1').html("<thead><tr><th>课程名称</th><th>未交作业</th><th>未读公告</th><th>新文件</th></tr></thead>" + $('#info_1').html());
+    for (var i = 2; i < $('#info_1 tbody tr').length; i++) {
+      for (var j = 1; j < 4; j++) {
+        $('#info_1 tbody tr:eq(' + i + ') td:eq(' + j + ')').html($('#info_1 tbody tr:eq(' + i + ') td:eq(' + j + ')').html().split('个')[0]);
+      }
+    }
   }
 });
